da11d75167bd8bb3cba62db461a1fc99ee77f09a
GrigoriiKushnir <gris.kushnir@gmail.com>
2025-10-24 19:18:05 +0300

    preload

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ AWS_SERVICE = 'es'
 AWS_ROLE_ARN = 'arn:aws:iam::080274686453:role/sagemaker_admin'
 
 HF_MODEL_INSTANCE_TYPE = 'ml.c5.xlarge'
-SAGEMAKER_ENDPOINT_NAME = f"jobbert3-{HF_MODEL_INSTANCE_TYPE.replace('.', '-')}-ecr-self-host"
+SAGEMAKER_ENDPOINT_NAME = f"jobbertv3-{HF_MODEL_INSTANCE_TYPE.replace('.', '-')}"
 
 OS_DOMAIN_HOST = 'localhost'
 OS_DOMAIN_PORT = 9201
